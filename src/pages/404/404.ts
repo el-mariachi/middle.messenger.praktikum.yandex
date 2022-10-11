8cea574cbb020ff88e8c62e8cae4c8f4b40b185f
Dmitri Sokolov <mitson.mbox@gmail.com>
2022-10-11 12:03:11 +0300

    feat: Separate data from view in 404 & 500

--- src/pages/404/404.ts
+++ src/pages/404/404.ts
@@ -1,1 +1,14 @@
 import './404.scss';
+import { compile } from "handlebars";
+import { renderPage } from "../../utils/renderPage";
+
+import {errorPage} from "../../components/ErrorPage/ErrorPage.tmpl";
+
+const pageData = {
+    errorCode: 404,
+    errorMessage: 'Страница не найдена'
+};
+
+const template = compile(errorPage);
+
+renderPage('#app', template(pageData));
--- src/pages/500/500.ts
+++ src/pages/500/500.ts
@@ -1,1 +1,14 @@
 import './500.scss';
+import { compile } from "handlebars";
+import { renderPage } from "../../utils/renderPage";
+
+import {errorPage} from "../../components/ErrorPage/ErrorPage.tmpl";
+
+const pageData = {
+    errorCode: 500,
+    errorMessage: 'Ошибка сервера'
+};
+
+const template = compile(errorPage);
+
+renderPage('#app', template(pageData));
