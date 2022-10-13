b07571ecd94c95de601eba8c25c874e4e38708e0
Dmitri Sokolov <mitson.mbox@gmail.com>
2022-10-13 15:45:58 +0300

    refactor: Split .handlebars & .hbs files begin

--- src/pages/404/404.ts
+++ src/pages/404/404.ts
@@ -1,14 +1,15 @@
 import './404.scss';
-import { compile } from "handlebars";
-import { renderPage } from "../../utils/renderPage";
+import { compile } from 'handlebars';
+import { renderPage } from '../../utils/renderPage';
 
-import {errorPage} from "../../components/ErrorPage/ErrorPage.tmpl";
+import ErrorPage from 'bundle-text:../../components/ErrorPage/ErrorPage.hbs';
+console.log(ErrorPage);
 
 const pageData = {
-    errorCode: 404,
-    errorMessage: 'Страница не найдена'
+  errorCode: 404,
+  errorMessage: 'Страница не найдена',
 };
 
-const template = compile(errorPage);
+const template = compile(ErrorPage);
 
 renderPage('#app', template(pageData));
