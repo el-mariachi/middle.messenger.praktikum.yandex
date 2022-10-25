bf50cb8153043e9ce86d70ff897a0b7602f696db
Dmitri Sokolov <mitson.mbox@gmail.com>
2022-10-25 08:59:31 +0300

    feat: Complete HTTPTransport class

--- src/utils/HTTPTransport.ts
+++ src/utils/HTTPTransport.ts
@@ -6,28 +6,88 @@ enum METHOD {
   DELETE = 'DELETE',
 }
 
+interface IHeaders {
+  [k: string]: string;
+}
+
+interface IRequestBody {
+  [k: string]: unknown;
+}
+
 type Options = {
   method: METHOD;
-  headers?: {
-    [k: string]: string;
-  };
+  headers?: IHeaders;
   timeout?: number;
-  data?: unknown;
+  data?: IRequestBody;
 };
 
 type OptionsWithoutMethod = Omit<Options, 'method'>;
 
+function parseOptions(options: Options) {
+  const { method } = options;
+  let { headers, data } = options;
+  if (!headers || Object.keys(headers).length === 0) {
+    headers = undefined;
+  }
+  if (!data || Object.keys(data).length === 0) {
+    data = undefined;
+  }
+  return { method, headers, data };
+}
+
+function queryStringify(data: IRequestBody) {
+  return Object.entries(data)
+    .map(([key, value]) => `${key}=${encodeURIComponent(String(value))}`)
+    .join('&');
+}
 export class HTTPTransport {
   get(url: string, options: OptionsWithoutMethod = {}): Promise<XMLHttpRequest> {
-    return this.request(url, { ...options, method: METHOD.GET });
+    return this.request(url, { ...options, method: METHOD.GET }, options.timeout);
   }
 
-  request(url: string, options: Options = { method: METHOD.GET }): Promise<XMLHttpRequest> {
-    const { method, data } = options;
+  post(url: string, options: OptionsWithoutMethod = {}): Promise<XMLHttpRequest> {
+    return this.request(url, { ...options, method: METHOD.POST }, options.timeout);
+  }
+
+  put(url: string, options: OptionsWithoutMethod = {}): Promise<XMLHttpRequest> {
+    return this.request(url, { ...options, method: METHOD.PUT }, options.timeout);
+  }
+
+  patch(url: string, options: OptionsWithoutMethod = {}): Promise<XMLHttpRequest> {
+    return this.request(url, { ...options, method: METHOD.PATCH }, options.timeout);
+  }
+
+  delete(url: string, options: OptionsWithoutMethod = {}): Promise<XMLHttpRequest> {
+    return this.request(url, { ...options, method: METHOD.DELETE }, options.timeout);
+  }
+
+  request(
+    url: string,
+    options: Options = { method: METHOD.GET },
+    timeout: number | undefined
+  ): Promise<XMLHttpRequest> {
+    if (!url) {
+      throw new Error('Bad request url');
+    }
+    const { method, headers, data } = parseOptions(options);
+
+    if (method === METHOD.GET && data) {
+      url = `${url}?${queryStringify(data)}`;
+    }
 
     return new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
       xhr.open(method, url);
+      // set headers if any
+      if (headers) {
+        Object.entries(headers).forEach(([headerName, headerValue]) => {
+          xhr.setRequestHeader(headerName, headerValue);
+        });
+      }
+
+      if (timeout) {
+        xhr.timeout = timeout;
+      }
 
       xhr.onload = function () {
         resolve(xhr);
