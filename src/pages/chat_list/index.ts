c71ba789f43a78e0ef15c1f455f477e228c563f3
Dmitri Sokolov <mitson.mbox@gmail.com>
2022-11-04 18:14:03 +0300

    chore: Clean up style imports

--- src/pages/chat_list/index.ts
+++ src/pages/chat_list/index.ts
@@ -1,9 +1,8 @@
 import './chat_list.scss';
-import './chat_list.scss';
 import { ChatListPage } from './chat_list';
-import { renderDOM } from '../../utils/renderDOM';
-import ChatList from '../../components/ChatList';
-import MessageArea from '../../components/MessageArea';
+import { renderDOM } from '~/src/utils/renderDOM';
+import ChatList from '~/src/components/ChatList';
+import MessageArea from '~/src/components/MessageArea';
 
 const chatList = new ChatList({});
 const chatArea = new MessageArea({});
