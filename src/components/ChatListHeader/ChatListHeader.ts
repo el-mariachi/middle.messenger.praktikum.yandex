19846740ca7928a8eee3d155c785f5e9ffba7338
Dmitri Sokolov <mitson.mbox@gmail.com>
2022-11-04 15:54:46 +0300

    feat: Add Menu component

--- src/components/ChatListHeader/ChatListHeader.ts
+++ src/components/ChatListHeader/ChatListHeader.ts
@@ -1,6 +1,36 @@
 import { Block, IProps } from '~/src/classes/Block';
 import headerTemplate from './ChatListHeader.hbs';
+import Menu from '~/src/components/Menu';
+import Button from '~/src/components/Button';
 import Link from '~/src/components/Link';
+
+const renameLink = new Button('button', {
+  text: 'Переименовать',
+  classList: ['Menu-Link', 'Menu_linktype_edit'],
+});
+
+const deleteLink = new Button('button', {
+  text: 'Удалить',
+  classList: ['Menu-Link', 'Menu_linktype_delete'],
+});
+
+const chatListMenuData: IProps = {
+  classList: ['ChatListHeader-Menu'],
+  menuItems: [renameLink, deleteLink],
+  bodyType: 'Menu_bodytype_chatlist',
+};
+
+const chatListMenu = new Menu(chatListMenuData);
+
+const newChatButtonData = {
+  tagName: 'button',
+  attributes: {
+    type: 'button',
+  },
+  classList: ['NewChat', 'ChatListHeader-NewChat', 'Icon', 'Icon_type_newchat'],
+};
+
+const newChatButton = new Button(newChatButtonData.tagName, newChatButtonData);
 
 const profileUrl = new URL('/src/pages/edit_profile/edit_profile.handlebars', import.meta.url);
 
@@ -15,7 +45,7 @@ const profileLink = new Link({
 export class ChatListHeader extends Block {
   constructor(props: IProps) {
     const classList = ChatListHeader.appendClassList(['ChatListHeader'], props);
-    super('div', { ...props, classList, profileLink });
+    super('div', { ...props, classList, chatListMenu, newChatButton, profileLink });
   }
   render(): DocumentFragment {
     return this.compile(headerTemplate, this.props);
--- src/pages/chat_list/chat_list.ts
+++ src/pages/chat_list/chat_list.ts
@@ -1,12 +1,21 @@
 import './chat_list.scss';
 import { Block, IProps } from '../../classes/Block';
 import pageTemplate from './chat_list.hbs';
+import { EventBusSingl } from '~/src/controllers/EventBusSingl';
+import { EVENTS } from '~/src/constants/events';
+
+const appBus = new EventBusSingl();
 
 export class ChatListPage extends Block {
   constructor(props: IProps) {
     const classList = ChatListPage.appendClassList(['Page', 'Page_type_chatlist'], props);
     const settings = { hasID: true };
     super('div', { ...props, classList, settings });
+  }
+  componentDidMount(): void {
+    this._element.addEventListener('click', (evt: Event) => {
+      appBus.emit(EVENTS.PAGE_CLICK, evt);
+    });
   }
   render(): DocumentFragment {
     return this.compile(pageTemplate, this.props);
