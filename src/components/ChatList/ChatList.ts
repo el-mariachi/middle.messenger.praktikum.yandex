93005d86a0a01f10d390ef79824207d199b00a21
Dmitri Sokolov <mitson.mbox@gmail.com>
2022-11-22 12:10:01 +0300

    refactor: Move modal into ChatList component

--- src/components/ChatList/ChatList.ts
+++ src/components/ChatList/ChatList.ts
@@ -1,12 +1,29 @@
-import { Block, IProps } from '../../classes/Block';
+import { Block } from '../../classes/Block';
+import { PageProps } from '../Page';
 import chatListTemplate from './ChatList.hbs';
+import Modal from '../Modal';
+import { ChatListController } from '../../controllers/ChatListController';
+import { EventBusSingl } from '../../controllers/EventBusSingl';
+import { EVENTS } from '../../constants/events';
+
+const appBus = new EventBusSingl();
+
+function createPageResources(currentPath: string) {
+  const pageModal = new Modal({});
+  new ChatListController(currentPath, pageModal);
+  const click = (evt: Event) => {
+    appBus.emit(EVENTS.PAGE_CLICK, evt);
+  };
+  return { pageModal, click };
+}
 
 export class ChatList extends Block {
-  constructor(props: IProps) {
+  constructor(props: PageProps) {
     const tagName = 'aside';
     const classList = ChatList.appendClassList(['ChatList'], props);
     const settings = { hasID: true };
-    super({ ...props, tagName, classList, settings });
+    const { pageModal, click } = createPageResources(props.currentPath);
+    super({ ...props, tagName, classList, settings, pageModal, events: { click } });
   }
   render(): DocumentFragment {
     return this.compile(chatListTemplate, this.props);
--- src/pages/chat_list/chat_list.ts
+++ src/pages/chat_list/chat_list.ts
@@ -1,29 +1,13 @@
 import Page, { PageProps } from '../../components/Page';
 import pageTemplate from './chat_list.hbs';
-import { EventBusSingl } from '../../controllers/EventBusSingl';
-import { EVENTS } from '../../constants/events';
 import chatList from '../../components/ChatList';
 import messageArea from '../../components/MessageArea';
-import { ChatListController } from '../../controllers/ChatListController';
-import Modal from '../../components/Modal';
-
-const appBus = new EventBusSingl();
-
-function createPageResources(currentPath: string) {
-  const pageModal = new Modal({});
-  new ChatListController(currentPath, pageModal);
-  const click = (evt: Event) => {
-    appBus.emit(EVENTS.PAGE_CLICK, evt);
-  };
-  return { pageModal, click };
-}
 
 export class ChatListPage extends Page {
   constructor(props: PageProps) {
     const classList = ChatListPage.appendClassList(['Page', 'Page_type_chatlist'], props);
     const settings = { hasID: true };
-    const { pageModal, click } = createPageResources(props.currentPath);
-    super({ ...props, classList, settings, chatList, messageArea, pageModal, events: { click } });
+    super({ ...props, classList, settings, chatList, messageArea });
   }
   render(): DocumentFragment {
     this.setPageTitle('Chat List');
