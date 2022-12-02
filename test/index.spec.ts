fda1746ac5457207576da0c1383df85cf0df1b5e
Dmitri Sokolov <mitson.mbox@gmail.com>
2022-12-02 10:22:07 +0300

    test: Prepare for Jest, remove mocha

--- test/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-
-import { hello } from './';
-
-describe('Typescript + Babel usage suite', () => {
-  it('should return string correctly', () => {
-    expect(hello('mocha'), 'Hello mocha');
-  });
-});
